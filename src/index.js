aa1723aafc849289813fe789ea394baf15d593ec
Jean Urena <juanpedro1519@gmail.com>
2021-01-16 16:25:37 -0400

    Modern Javascript Setup with Webpack and Babel

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { test } from './test';
+
+const variable = 'Webpack Project3';
+
+test();
+console.log(`Hello ${variable}`);
+
+
+class Person {
+  constructor() {
+    this.name = 'jean';
+  }
+}
+
+const object = new Person();
+console.log(object.name);
+
