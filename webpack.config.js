4c9126a9f959626ec247cb72ec4e1e3a60d6c8d6
Jean Urena <juanpedro1519@gmail.com>
2021-01-17 11:42:09 -0400

    Modifying JS Paths

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, './src/js/index.js'),
   module: {
     rules: [
       {
@@ -23,7 +23,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    filename: 'js/bundle.js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, './dist')
